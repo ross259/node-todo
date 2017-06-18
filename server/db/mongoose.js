4e7d7d385dcebdac30d9e0ee9cb52e9be275bccf
Ross259 <ross.burke@gmail.com>
2017-06-17 19:54:07 -0600

    Configured mongoose to read connection string from environment variable.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
 //mongoose.connect('mongodb://ross259:w0Rd2urM0M!@ds143181.mlab.com:43181/node_todo259');
+
 //mongoose.connect('mongodb://ross259:w0Rd2urM0M!@ds131312.mlab.com:31312/todo2_259')
 
 module.exports = {
